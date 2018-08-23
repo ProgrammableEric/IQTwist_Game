b69537421aff4e458c7523f571ac68d844f68206
u5885811 <u5885811@anu.edu.au>
2018-08-23 19:31:11 +1000

    yeh!

--- tests/gittest/B.java
+++ tests/gittest/B.java
@@ -4,7 +4,7 @@ public class B {
     public String toString (){
 
         String a = "here's a change I made!";
-        String b = "here's another change I made!";
+//        String b = "here's another change I made!";
 
 
         return null;
--- tests/gittest/Main.java
+++ tests/gittest/Main.java
@@ -2,6 +2,8 @@ package gittest;
 
 public class Main {
     public static void main(String[] args) {
+        B b = new B();
+
 
     }
 }
